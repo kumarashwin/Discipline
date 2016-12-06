d6c6b45c79bb220fc3ad45cb0c21d6e57ed4dcbd
Ash Kumar <kumar.ashwin@outlook.com>
2016-12-06 16:24:09 -0500

    Created a partial view for the menu that will be used to edit|delete activities

---
+++ Tache.Web/Scripts/activitiesRightClick.js
@@ -0,0 +1,10 @@
+﻿var activitiesCRUD = document.getElementById("activities-CRUD");
+var backgroundDiv = document.getElementById("background-cover");
+
+var activitiesList = document.getElementsByClassName("activities-list")[0].querySelector("ul");
+
+activitiesList.addEventListener("contextmenu", function (event) {
+    event.stopPropagation();
+    event.preventDefault();
+    activitiesCRUD.style.display = "block";
+});
