c31494a87021b90bbbea72a3a9cb0a319c655332
Ash Kumar <kumar.ashwin@outlook.com>
2016-12-08 13:48:53 -0500

    Delete activity button disappears when adding new activity. Also cleaned code and added X button

--- Tache.Web/Scripts/activitiesRightClick.js
+++ Tache.Web/Scripts/activitiesRightClick.js
@@ -1,15 +1,16 @@
 ﻿function setupActivitiesCRUD() {
     var activitiesCRUD = document.getElementById("activities-CRUD");
-    var backgroundDiv = document.getElementById("background-cover");
 
     var activitiesList = document.getElementsByClassName("activities-list")[0].querySelector("ul");
     var newButton = document.getElementById("new-activity");
+    var deleteButton = activitiesCRUD.querySelector("form.delete");
 
     activitiesList.addEventListener("contextmenu", activityCRUDEventHandler);
     newButton.addEventListener("click", activityCRUDEventHandler);
 
     function activityCRUDEventHandler(event) {
         if (event.target.nodeName == "BUTTON") {
+            deleteButton.style.display = "none";
             var inputs = activitiesCRUD.querySelectorAll("input");
             inputs.forEach(function (input) { input.value = ""; });
 
@@ -17,6 +18,7 @@
                 inputs.forEach(function (input) {
                     input.value = event.target.getAttribute("data-activity-" + input.id.toLowerCase());
                 });
+                deleteButton.style.display = "block";
             }
             event.preventDefault();
             activitiesCRUD.style.display = "block";
