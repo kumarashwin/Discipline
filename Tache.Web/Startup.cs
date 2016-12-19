63b34c5b3dc6b0cf04cf26a1ac60bfc51adc98fd
Ash Kumar <kumar.ashwin@outlook.com>
2016-12-19 18:51:49 -0500

    Made some commit mistake here

---
+++ Tache.Web/Startup.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Tache.Web.Startup))]
+namespace Tache.Web {
+    public partial class Startup {
+        //public void Configuration(IAppBuilder app) {
+        //    ConfigureAuth(app);
+        //}
+    }
+}
