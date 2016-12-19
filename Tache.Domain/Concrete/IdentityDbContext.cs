73ae23d968409b6b857951b6a0b9bd98ad0dc4ad
Ash Kumar <kumar.ashwin@outlook.com>
2016-12-19 18:59:38 -0500

    Laid the foundation for implementing Identity. Still doesn't create tables in DB

---
+++ Tache.Domain/Concrete/IdentityDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using Tache.Domain.Entities;
+
+namespace Tache.Domain.Models {
+
+    public class IdentityDbContext : IdentityDbContext<TacheUser> {
+        public IdentityDbContext() : base("Tache", throwIfV1Schema: false) { }
+        public static IdentityDbContext Create() => new IdentityDbContext();
+    }
+}
---
+++ Tache.Domain/Entities/TacheUser.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Tache.Domain.Entities {
+    public class TacheUser : IdentityUser {
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<TacheUser> manager) {
+
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+
+            // Add custom user claims here
+            return userIdentity;
+        }
+    }
+}
